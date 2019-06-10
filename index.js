a681a9989a5221efeaa8e64b9100c2c6ed2e9c9c
Julio <julio.olvr@gmail.com>
2019-06-10 08:38:57 +0200

    Try to validate Slack request

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 
+const validateSlackRequest = require("./slackValidationMiddleware");
 const handlers = require("./voteHandler");
 
 const app = express();
 const port = 3000; // TODO: Env var
 
 // TODO: Request validation
+app.use(validateSlackRequest);
 app.use(express.urlencoded({ extended: false }));
 app.post("/start", handlers.handleStart);
 app.post("/vote", handlers.handleAction);
