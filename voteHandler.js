b0d97cafc55a40ef3ea7df0d3c0123cd993b06a0
Julio <julio.olvr@gmail.com>
2019-06-17 23:25:11 +0200

    Some better formatting

--- voteHandler.js
+++ voteHandler.js
@@ -1,5 +1,15 @@
 const uuid = require('uuid/v4')
-const { splitEvery, pipe, append, flip, concat } = require('ramda')
+const {
+  splitEvery,
+  pipe,
+  append,
+  flip,
+  concat,
+  when,
+  not,
+  isEmpty,
+  ifElse
+} = require('ramda')
 
 const respondTo = require('./respondToSlack')
 
@@ -46,7 +56,7 @@ function acknowledge (res) {
 }
 
 // TODO: Configurable options
-const OPTIONS = [0, 1, 2, 3, 5, 8, '∞', '?']
+const OPTIONS = ['0', '1', '2', '3', '5', '8', '∞', '?']
 
 const runningVotes = {}
 
@@ -61,12 +71,37 @@ function startVotesFor (storyName) {
   )([])
 }
 
+function voteInProgressFor (id, vote) {
+  return pipe(
+    addTitle(vote.storyName),
+    addVotes(id, OPTIONS),
+    addVoters(vote.votes),
+    addCloseVote(id)
+  )([])
+}
+
+function closedVoteFor (id, vote) {
+  return pipe(
+    addClosedTitle(vote.storyName),
+    addResults(vote.votes, OPTIONS)
+  )([])
+}
+
 const addTitle = title =>
   append({
     type: 'section',
     text: {
       type: 'mrkdwn',
-      text: title
+      text: `*Voting* :point_right: ${title}`
+    }
+  })
+
+const addClosedTitle = title =>
+  append({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `*Voting closed* :checkered_flag: ${title}`
     }
   })
 
@@ -102,6 +137,57 @@ const addCloseVote = voteId =>
     ]
   })
 
+const addVoters = votes =>
+  when(
+    pipe(
+      isEmpty,
+      not
+    ),
+    append({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `Already voted: ${Object.keys(votes)
+          .map(voterId => `<@${voterId}>`)
+          .join(', ')}`
+      }
+    })
+  )
+
+const hasVotes = votes => option => votesCountFor(votes, option) > 0
+
+const votesCountFor = (votes, option) => votersFor(votes, option).length
+
+const votersFor = (votes, option) =>
+  Object.keys(votes).filter(voterId => votes[voterId] === option)
+
+const addResults = (votes, options) =>
+  ifElse(
+    () => not(isEmpty(votes)),
+    append({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: options
+          .filter(hasVotes(votes))
+          .map(
+            option =>
+              `• ${option} - ${votersFor(votes, option)
+                .map(voterId => `<@${voterId}>`)
+                .join(', ')}`
+          )
+          .join('\n')
+      }
+    }),
+    append({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: ':cry: No votes'
+      }
+    })
+  )
+
 function countVote (id, option, payload) {
   // TODO: Handle non-existing vote properly
   const votes = runningVotes[id].votes
@@ -119,75 +205,7 @@ function closeVote (id) {
   vote.closed = true
 }
 
-function buildMessage (id, storyName) {
-  const content = []
+function buildMessage (id) {
   const vote = runningVotes[id]
-
-  content.push({
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: vote.storyName
-    }
-  })
-
-  if (!vote.closed) {
-    splitEvery(4, OPTIONS).map(optionsSlice => {
-      content.push({
-        type: 'actions',
-        elements: optionsSlice.map(option => ({
-          type: 'button',
-          text: {
-            type: 'plain_text',
-            text: String(option)
-          },
-          action_id: uuid(), // TODO: Is it really needed?
-          value: `${option}.${id}`
-        }))
-      })
-    })
-
-    content.push({
-      type: 'actions',
-      elements: [
-        {
-          type: 'button',
-          text: {
-            type: 'plain_text',
-            text: 'Close vote'
-          },
-          action_id: uuid(),
-          value: `close.${id}`
-        }
-      ]
-    })
-
-    const voters = Object.keys(vote.votes)
-
-    if (voters.length > 0) {
-      content.push({
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `Already voted: ${voters
-            .map(voterId => `<@${voterId}>`)
-            .join(', ')}`
-        }
-      })
-    }
-  } else {
-    const voters = Object.keys(vote.votes)
-
-    content.push({
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: `Voting is closed! ${voters.map(
-          voterId => `<@${voterId}>: ${vote.votes[voterId]}`
-        )}`
-      }
-    })
-  }
-
-  return content
+  return vote.closed ? closedVoteFor(id, vote) : voteInProgressFor(id, vote)
 }
